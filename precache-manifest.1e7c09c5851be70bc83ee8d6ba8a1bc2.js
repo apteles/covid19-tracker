3f577d415cdd13fcdd0eb679041f2506d9fbd52c
Andre Teles <andre.telestp@gmail.com>
2020-08-14 16:28:08 -0300

    Updates

---
+++ precache-manifest.1e7c09c5851be70bc83ee8d6ba8a1bc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "137ed56ac50f97e43012da99d53952f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "857906b4d65da841c24c",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "857906b4d65da841c24c",
+    "url": "/static/js/2.b6afdf0b.chunk.js"
+  },
+  {
+    "revision": "9946d09cf1d000b4a165711edf4119ce",
+    "url": "/static/js/2.b6afdf0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64c520fb2b8e7780af04",
+    "url": "/static/js/main.3fb74cfc.chunk.js"
+  },
+  {
+    "revision": "e663df2881e39a90859c",
+    "url": "/static/js/runtime-main.225c0f6f.js"
+  },
+  {
+    "revision": "9df79656ea7afa627bb9a3d8180a0e2c",
+    "url": "/static/media/logo2.9df79656.png"
+  }
+]);
